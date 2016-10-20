772eca4ff73764a4203e00cd16acdc27981621b4
Gaurav Kumar <gaurav@redmart.com>
2016-10-20 21:04:48 +0530

    git ignore added

--- tutorial/src/helloworld.js
+++ tutorial/src/helloworld.js
@@ -1,4 +1,19 @@
-ReactDOM.render(
-<h1>Hello, world!</h1>,
-    document.getElementById('example')
-);
+class HelloWorld extends React.Component{
+    render(){
+        return(
+            <p>
+                Hello, <input type="text" placeholder="Your name here" /> !
+                It is {this.props.date.toTimeString() }
+            </p>
+        );
+    }
+}
+
+function tick() {
+    ReactDOM.render(
+        <HelloWorld date={new Date()}/>,
+        document.getElementById('example')
+    );
+}
+
+setInterval(tick, 500);
